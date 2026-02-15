bef4a83389ee838ed384d367c5603f21a04f171f
Alexis Lopez <alexislopezperez2003@gmail.com>
2026-02-14 19:41:50 -0600

    Aplicacion del tema de Material Design sin la tipografia

--- app/src/main/java/com/example/pizza/MainActivity.kt
+++ app/src/main/java/com/example/pizza/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,7 +15,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,7 +27,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,27 +34,33 @@ import androidx.compose.ui.unit.dp
 import com.example.pizza.Modelo.pizza
 import com.example.pizza.datos.Datos
 import com.example.pizza.ui.theme.PizzaTheme
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PizzaTheme {
-                PizzaApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PizzaApp(Modifier.padding(innerPadding))
+                }
             }
         }
     }
 }
 
 @Composable
-fun PizzaApp(){
+fun PizzaApp(modifier: Modifier = Modifier){
     ListaDePizzas(pizzaList = Datos().loadPizzas())
 }
 
 @Composable
 fun PizzaCard(pizza: pizza, modifier: Modifier = Modifier){
     var cant by remember { mutableIntStateOf(0) }
-    Card(modifier = modifier) {
+    Card(modifier = modifier,
+        colors = CardDefaults.cardColors(
+            contentColor = MaterialTheme.colorScheme.primaryContainer
+        )) {
         Column {
             Image(painter = painterResource(pizza.imageResourceId),
                 contentDescription = stringResource(pizza.stringResourceId),
@@ -60,14 +68,15 @@ fun PizzaCard(pizza: pizza, modifier: Modifier = Modifier){
                 contentScale = ContentScale.Crop)
 
             Text(modifier = modifier.padding(16.dp),
-                text = LocalContext.current.getString(pizza.stringResourceId),
-                style = MaterialTheme.typography.headlineSmall,)
+                text = stringResource(pizza.stringResourceId),
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onPrimaryContainer)
 
             Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically){
 
                 Button(modifier = modifier, onClick = {if(cant>0) cant--}) { Text("-") }
 
-                Text(text= "$cant")
+                Text(text= "$cant", style = MaterialTheme.typography.headlineSmall, color = MaterialTheme.colorScheme.onPrimaryContainer)
 
                 Button(modifier = modifier, onClick = {cant++}) { Text("+") }
 
@@ -77,10 +86,12 @@ fun PizzaCard(pizza: pizza, modifier: Modifier = Modifier){
     }
 }
 
-@Preview
+@Preview (showBackground = true)
 @Composable
 private fun PizzaCardPreview(){
-    PizzaApp()
+    PizzaTheme{
+        PizzaApp()
+    }
 }
 
 @Composable
